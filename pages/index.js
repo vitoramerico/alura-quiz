46f01d8afa6534b892af7b3edd5bba64f68f80a8
vitoramerico <vitor_americo@hotmail.com>
2021-01-25 15:01:49 -0300

    codigo final da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,42 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
+import BoxWidget from "../src/components/boxWidget";
+import Footer from "../src/components/footer";
+import GitHubCorner from "../src/components/gitHubCorner";
+import QuizBackground from "../src/components/quizBackground";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <BoxWidget>
+          <BoxWidget.Header>
+            <h1>The legend of zelda</h1>
+          </BoxWidget.Header>
+          <BoxWidget.Content>
+            <p>lorem ipsum dolor sit amet..</p>
+          </BoxWidget.Content>
+        </BoxWidget>
+        <BoxWidget>
+          <BoxWidget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>lorem ipsum dolor sit amet..</p>
+          </BoxWidget.Content>
+        </BoxWidget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="" />
+    </QuizBackground>
+  );
 }
